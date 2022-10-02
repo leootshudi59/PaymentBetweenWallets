2b1c8dfff09a615dde7c68c1c25249b17cd6282a
Léo OTSHUDI <leootshudi@gmail.com>
2022-10-02 20:13:26 +0200

    style: added Activity navigation bar

--- src/components/activity/ActivityCard.jsx
+++ src/components/activity/ActivityCard.jsx
@@ -1,5 +1,28 @@
-function ActivityCard() {
-  return <div></div>
+import {GlobeIcon, UserIcon, UserGroupIcon} from '@heroicons/react/outline';
+import styles from '../../styles/Activity.module.css'
+
+const ActivityCard = () => {
+  return (
+  <div className={ styles.container }>
+    <div className={ styles.tabContainer}>
+      <h3 className={ styles.tabTitle}>Activity</h3>
+      <div className={ styles.navigationContainer }>
+        <div className={ styles.navigationItem } data-current>
+          <GlobeIcon className={ styles.navigationIcon } />
+        </div>
+        <div className={ styles.navigationItem }>
+          <UserIcon className={ styles.navigationIcon } />
+        </div>
+        <div className={ styles.navigationItem }>
+          <UserGroupIcon className={ styles.navigationIcon } />
+        </div>
+      </div>
+    </div>
+    <div className={ styles.feedList }>
+      To implement here
+    </div>
+  </div>
+  )
 }
 
-export default ActivityCard
+export default ActivityCard;
